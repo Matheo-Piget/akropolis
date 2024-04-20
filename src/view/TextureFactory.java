4f0e966b001d9a7dbaa5289d6feb0cbd7157dc1e
matheo <matheo.piget5@gmail.com>
2024-04-20 13:56:22 +0200

    tentative de chargement de toutes les textures mais les couleur ne sont pas assez marqué a mon avis, a voir

--- src/view/TextureFactory.java
+++ src/view/TextureFactory.java
@@ -6,6 +6,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.awt.Graphics2D;
+import java.util.Objects;
 
 /** This class is a factory for loading images. */
 public class TextureFactory {
@@ -13,6 +14,7 @@ public class TextureFactory {
 
     public static BufferedImage getTexture(String imageName) {
         BufferedImage texture = textures.get(imageName);
+        System.out.println(textures);
         if (texture != null) {
             return texture;
         }
@@ -21,47 +23,69 @@ public class TextureFactory {
         Graphics2D g = img.createGraphics();
         switch (imageName) {
             case "quarries":
-                Color gray = new Color(128, 128, 128);
-                // Then fill a TexturePaint with the color
-                g.setColor(gray);
-                g.fillRect(0, 0, 1, 1);
+                try {
+                    img = ImageIO.read(Objects.requireNonNull(TextureFactory.class.getResourceAsStream("/quarries.png")));
+                } catch (IOException e) {
+                    Color gray = new Color(128, 128, 128);
+                    // Then fill a TexturePaint with the color
+                    g.setColor(gray);
+                    g.fillRect(0, 0, 1, 1);
+                }
                 break;
             case "barrack":
-                Color red = new Color(255, 0, 0);
-                // Then fill a TexturePaint with the color
-                g.setColor(red);
-                g.fillRect(0, 0, 1, 1);
-                g.dispose();
+                try {
+                    img = ImageIO.read(Objects.requireNonNull(TextureFactory.class.getResourceAsStream("/barrack.png")));
+                } catch (IOException e) {
+                    Color red = new Color(255, 0, 0);
+                    // Then fill a TexturePaint with the color
+                    g.setColor(red);
+                    g.fillRect(0, 0, 1, 1);
+                    g.dispose();
+                }
+                break;
             case "building":
-                Color blue = new Color(0, 0, 255);
-                // Then fill a TexturePaint with the color
-                g.setColor(blue);
-                g.fillRect(0, 0, 1, 1);
+                try {
+                    img = ImageIO.read(Objects.requireNonNull(TextureFactory.class.getResourceAsStream("/building.png")));
+                } catch (IOException e) {
+                    Color blue = new Color(0, 0, 255);
+                    // Then fill a TexturePaint with the color
+                    g.setColor(blue);
+                    g.fillRect(0, 0, 1, 1);
+                }
                 break;
             case "garden":
-                Color green = new Color(0, 255, 0);
-                // Then fill a TexturePaint with the color
-                g.setColor(green);
-                g.fillRect(0, 0, 1, 1);
+                try {
+                    img = ImageIO.read(Objects.requireNonNull(TextureFactory.class.getResourceAsStream("/garden.png")));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Color green = new Color(0, 255, 0);
+                    // Then fill a TexturePaint with the color
+                    g.setColor(green);
+                    g.fillRect(0, 0, 1, 1);
+                }
                 break;
             case "market":
                 // Load the image
                 try {
-                    img = ImageIO.read(TextureFactory.class.getResourceAsStream("/market.png"));
+                    img = ImageIO.read(Objects.requireNonNull(TextureFactory.class.getResourceAsStream("/market.png")));
                 } catch (IOException e) {
                     e.printStackTrace();
                     Color yellow = new Color(255, 255, 0);
                     // Then fill a TexturePaint with the color
                     g.setColor(yellow);
                     g.fillRect(0, 0, 1, 1);
-                    break;
                 }
                 break;
             case "temple":
-                Color purple = new Color(255, 0, 255);
-                // Then fill a TexturePaint with the color
-                g.setColor(purple);
-                g.fillRect(0, 0, 1, 1);
+                try {
+                    img = ImageIO.read(Objects.requireNonNull(TextureFactory.class.getResourceAsStream("/temple.png")));
+                } catch (IOException e) {
+                    Color purple = new Color(255, 0, 255);
+                    // Then fill a TexturePaint with the color
+                    g.setColor(purple);
+                    g.fillRect(0, 0, 1, 1);
+
+                }
                 break;
         }
         g.dispose();
