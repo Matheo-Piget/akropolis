74c89f49937b3d2871681a76fa90ca5ffa8f65be
Chetouani Bilal <chetouanibilal04@gmail.com>
2024-05-17 21:13:09 +0200

    Git capricieux supprime les fichiers on le remercie

--- run.ps1
+++ run.ps1
@@ -5,24 +5,22 @@ $BIN_DIR = "bin"
 $TEST_DIR = "src/test"
 
 # Delete all .class files in the bin directory
-Get-ChildItem -Path $BIN_DIR -Recurse -Include *.class | Remove-Item -Force -ErrorAction SilentlyContinue
-Write-Output "Deleted .class files in $BIN_DIR directory"
+Get-ChildItem -Path ./$BIN_DIR -Recurse -Include *.class | Remove-Item
+Write-Output "Delete success"
 
 # Create the bin directory if it doesn't exist
 if (!(Test-Path -Path $BIN_DIR)) {
-    New-Item -ItemType Directory -Path $BIN_DIR | Out-Null
-    Write-Output "Created $BIN_DIR directory"
+    New-Item -ItemType Directory -Path $BIN_DIR
 }
 
-# Get Java files in the source directory (excluding the test directory)
-$javaFiles = Get-ChildItem -Path $SRC_DIR -Recurse -Filter *.java | Where-Object { $_.DirectoryName -notmatch $TEST_DIR }
-
-# Compile Java files into the bin directory
+$javaFiles = @(Get-ChildItem -Path $SRC_DIR -Recurse -Filter *.java | Where-Object { $_.DirectoryName -notmatch $TEST_DIR })
 Write-Output "Number of Java files: $($javaFiles.Count)"
-foreach ($javaFile in $javaFiles) {
-    Write-Output "Compiling $($javaFile.FullName)"
+# Compile all Java files in the source directory (excluding the test directory) into the bin directory
+Write-Output "Compiling Java file $($javaFiles[0].FullName)"
+$compileOutput = $javaFiles | ForEach-Object {
+    Write-Output "Compiling"
     try {
-        $output = javac -cp $SRC_DIR -d $BIN_DIR $javaFile.FullName 2>&1
+        $output = javac -cp $SRC_DIR -d $BIN_DIR $_.FullName 2>&1
         Write-Output "javac output: $output"
     } catch {
         Write-Output "Error: $_"
@@ -30,8 +28,7 @@ foreach ($javaFile in $javaFiles) {
 }
 
 # If compilation was successful, run the main class
-if ($?) {
-    Write-Output "Compilation successful. Running the main class."
+if ($LASTEXITCODE -eq 0) {
     java -cp "$BIN_DIR;$RES_DIR" view.main.App
 } else {
     Write-Output "Compilation failed"
